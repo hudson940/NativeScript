71827fab55f707224b787e991f58ecb6d54ee73d
hudson940 <andersonvidal94@gmail.com>
2018-03-03 15:14:38 -0600

    Initial Commit

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  template: `
+    <ActionBar title="My App" class="action-bar"></ActionBar>
+    <!-- Your UI components go here -->
+    <Image src="~/images/apple.jpg"></Image>
+  `
+})
+export class AppComponent {
+  // Your TypeScript logic goes here
+}
